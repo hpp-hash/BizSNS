459c5ca7ce821048ef267e26ef7a0c4ecbf69d79
HungP <HungP>
2019-07-09 16:04:51 -0700

    modified the customDrawerContent

--- src/DrawerContentComponents.js
+++ src/DrawerContentComponents.js
@@ -11,37 +11,39 @@ export default class drawerContentComponents extends Component {
 
     render() {
         return(
-            <SafeAreaView style={{flex:1, alignItems: 'center', paddingTop: hp('10%')}}>
+            <SafeAreaView style={{flex:1, paddingTop: hp('10%')}}>
                 <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                     <Image style={{width: wp('12%'), height: hp('7%')}} source={require('../assets/placeholder_avatar.png')}/>
                     <Text style={[styles.text, {fontSize: wp('6%')}]}>
                     Welcome, Patrick
                     </Text>
                 </View>
-                <TouchableOpacity
-                    style={{margin: hp('1%')}}
-                    onPress={() => this.props.navigation.navigate('Profile')}
-                >
-                    <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Profile </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={{margin: hp('1%')}}
-                    onPress={() => this.props.navigation.navigate('WelcomeScreen')}
-                >
-                    <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Configure SNS </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={{margin: hp('1%')}}
-                    onPress={() => this.props.navigation.navigate('ChangePassword')}
-                >
-                    <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Change Password </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={{margin: hp('1%')}}
-                    onPress={() => this.props.navigation.navigate('Login')}
-                >
-                    <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Logout </Text>
-                </TouchableOpacity>
+                <View style={{flex: 1, alignItems: 'flex-start', paddingTop: hp('2%'), paddingLeft: wp('5%')}}>
+                    <TouchableOpacity
+                        style={{margin: hp('1%')}}
+                        onPress={() => this.props.navigation.navigate('Profile')}
+                    >
+                        <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Profile </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{margin: hp('1%')}}
+                        onPress={() => this.props.navigation.navigate('WelcomeScreen')}
+                    >
+                        <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Configure SNS </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{margin: hp('1%')}}
+                        onPress={() => this.props.navigation.navigate('ChangePassword')}
+                    >
+                        <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Change Password </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{margin: hp('1%')}}
+                        onPress={() => this.props.navigation.navigate('Login')}
+                    >
+                        <Text style={{fontSize: wp('5%'), textAlign: 'center'}}> Logout </Text>
+                    </TouchableOpacity>
+                </View>
             </SafeAreaView>
         )
     }
